be6f9275984ec34bab76ef799059c70f42dd9b17
Meeperty <meepprograms@gmail.com>
2021-08-30 15:17:14 -0700

    Add project files.

---
+++ SubnauticaMod/KnifeMod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using Logger = QModManager.Utility.Logger;
+
+namespace SubnauticaMod
+{
+    class KnifeMod
+    {
+        [HarmonyPatch(typeof(Knife))]
+        [HarmonyPatch("Start")]
+        internal class KnifeDamageMod
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Knife __instance)
+            {
+                float knifeDamage = __instance.damage;
+                float newKnifeDamage = knifeDamage * 5;
+                __instance.damage = newKnifeDamage;
+                Logger.Log(Logger.Level.Debug, $"Knife damage was {knifeDamage}, is now {newKnifeDamage}");
+            }
+        }
+    }
+}
---
+++ SubnauticaMod/Qmod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using Logger = QModManager.Utility.Logger;
+
+namespace SubnauticaMod
+{
+    [QModCore]
+    public class Qmod
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var modName = $"meeperty_{assembly.GetName().Name}";
+            Logger.Log(Logger.Level.Info, $"Patching {modName}");
+            Harmony harmony = new Harmony(modName);
+            harmony.PatchAll(assembly);
+            Logger.Log(Logger.Level.Info, "Patched Successfully");
+        }
+    }
+}
